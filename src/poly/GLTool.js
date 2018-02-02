bac532f2d2a6a8f9273f70c91f82241c603bd5fe
clément chenebault <clem.chenebault@gmail.com>
2018-02-02 15:08:30 +0000

    instancing! mesh.addAttribute(data, name, count, instance = true || false)

--- src/poly/GLTool.js
+++ src/poly/GLTool.js
@@ -10,12 +10,16 @@ export default new class GLTool
 		this.aspectRatio = 0;
 		this.state = null;
 		this.enabledVertexAttributes = [];
+
+		this.instancedAttributes = [];
 	}
 
 	init(gl)
 	{
 		this.state = new State(gl);
 		GLExtensions.init();
+		this._hasInstance = GLExtensions.checkExtension('ANGLE_instanced_arrays');
+		this._instanceExt = GLExtensions.getExtension('ANGLE_instanced_arrays');
 	}
 
 	_bindBuffers(mesh)
@@ -27,22 +31,26 @@ export default new class GLTool
 			let attrib = mesh._attributes[i];
 			gl.bindBuffer(gl.ARRAY_BUFFER, attrib.buffer);
 			let attribLocation = mesh.program.getAttributeLocation(attrib.name);
-
     		gl.vertexAttribPointer(attribLocation, attrib.itemSize, gl.FLOAT, false, 0, 0);
+
+			if(mesh.instanceCount > 0 && attrib.instance && this._hasInstance)
+			{
+				this._instanceExt.vertexAttribDivisorANGLE(attribLocation, 1);
+
+				this.instancedAttributes.push(attribLocation);
+			}
 
 			if(this.enabledVertexAttributes.indexOf(attribLocation) === -1)
             {
                 this.enabledVertexAttributes.push(attribLocation)
 				gl.enableVertexAttribArray(attribLocation); // NEVER FORGET THAT LINE (I did...)
 			}
-            // }
-
 		}
 
-		// if(mesh.indexBuffer)
-		// {
+		if(mesh.indexBuffer)
+		{
 			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.indexBuffer);
-		// }
+		}
 	}
 
 	setCamera(camera)
@@ -61,8 +69,13 @@ export default new class GLTool
 
 	draw(mesh)
 	{
+
 		if(this._lastMesh !== mesh)
 		{
+			// /!\ it's important to call it here because if there is only one instanced object on scene, it will be drawn only
+			// once, although we don't want to reset the instanced attributes :)
+			this.resetInstancedAttribute();
+
 			this._bindBuffers(mesh);
 			this._lastMesh = mesh;
 		}
@@ -86,13 +99,42 @@ export default new class GLTool
 			this._updateMatrices(mesh);
 		}
 
-		if(mesh.indexBuffer)
+
+
+		if(mesh.instanceCount > 0 && this._hasInstance)
+		{
+			if(mesh.indexBuffer)
+			{
+				this._instanceExt.drawElementsInstancedANGLE(mesh.drawType, mesh._indices.length, gl.UNSIGNED_SHORT, 0, mesh.instanceCount);
+			}
+			else
+			{
+				this._instanceExt.drawArraysInstancedANGLE(mesh.drawType, 0, mesh._numItems, mesh.instanceCount);
+			}
+		}
+		else if(mesh.indexBuffer)
 		{
 			gl.drawElements(mesh.drawType, mesh._indices.length, gl.UNSIGNED_SHORT, 0);
 		}
 		else
 		{
 			gl.drawArrays(mesh.drawType, 0, mesh._numItems);
+		}
+
+	}
+
+	// very important to reset the attributes location from one object to another as some of them
+	// could be not instanced, etc. but share the same attrib location
+	resetInstancedAttribute()
+	{
+		if(this.instancedAttributes.length > 0 && this._hasInstance)
+		{
+			for (var i = 0; i < this.instancedAttributes.length; i++) {
+				let attribLocation = this.instancedAttributes[i]
+				this._instanceExt.vertexAttribDivisorANGLE(attribLocation, 0);
+			}
+
+			this.instancedAttributes = [];
 		}
 	}
 
